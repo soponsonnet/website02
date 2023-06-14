786e4d1f5b023fb48bf9b21ddef54dfa572bc5d3
soponsonnet <sopon.sonnet@gmail.com>
2023-06-14 15:31:14 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="style.css" />
+
+<?php
+$name = "sopon sonnet";
+$age = 98;
+$year = 6652;
+$byear = $yaer - $age;
+echo "<h1> strongest </h1>";
+echo "by $name <br/>";
+echo "born $byear <br/>";
+?>
+<p>
+made by me
+</p>
+<img src="image/pic2.jpg"class="img2"/>
+<img src='images/ball.jpg' class='img1'  />
---
+++ login.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="style.css" />
+
+
+<h2 id="box1">  welcome  </h2>
+<form method="post" action="checklogin.php" enctype="multipart/form-data">
+    Username :
+    <input name="username" type="text" /> <br/> <br/>
+    Password :
+    <input name="password" type="password" /> <br/> <br/>
+    <input type="submit" value=" LOGIN " />
+</form>
+
+<a href="register.php"> Not yet applied </a>
